c49120129db415d4f53914f08995c899f7fb0ce1
Lachlan McDonald <lachlanmcdonald@users.noreply.github.com>
2024-07-29 16:15:05 +0100

    RF Removed through2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const PluginError = require('plugin-error');
-const through2 = require('through2');
 const replaceExtension = require('replace-ext');
 const path = require('path');
 const { pathToFileURL } = require('url');
 const applySourceMap = require('vinyl-sourcemaps-apply');
+const { Transform } = require('readable-stream');
 
 const PLUGIN_NAME = 'gulp-dartsass';
 
@@ -50,65 +50,66 @@ const gulpDartSass = (async, sass, options) => {
 		return callback(null, file);
 	};
 
-	const transformer = through2.obj((file, _encoding, callback) => {
-		// Ignore null files
-		if (file.isNull()) {
-			return callback(null, file);
-		}
+	return new Transform({
+		objectMode: true,
+		transform(file, _encoding, callback) {
+			// Ignore null files
+			if (file.isNull()) {
+				return callback(null, file);
+			}
 
-		// Streams are not supported
-		if (file.isStream()) {
-			return callback(new PluginError(PLUGIN_NAME, 'Streams not supported!'));
-		}
+			// Streams are not supported
+			if (file.isStream()) {
+				return callback(new PluginError(PLUGIN_NAME, 'Streams not supported!'));
+			}
 
-		// Ignore files beginning with an underscore
-		if (path.basename(file.path).startsWith('_')) {
-			return callback();
-		}
+			// Ignore files beginning with an underscore
+			if (path.basename(file.path).startsWith('_')) {
+				return callback();
+			}
 
-		// Ignore empty files
-		if (file.contents.length === 0) {
-			file.path = replaceExtension(file.path, '.css');
-			return callback(null, file);
-		}
+			// Ignore empty files
+			if (file.contents.length === 0) {
+				file.path = replaceExtension(file.path, '.css');
+				return callback(null, file);
+			}
 
-		const sassOptions = {
-			syntax: 'scss',
-			url: pathToFileURL(file.path).toString(),
-		};
+			const sassOptions = {
+				syntax: 'scss',
+				url: pathToFileURL(file.path).toString(),
+			};
 
-		// Generate Source Maps
-		sassOptions.sourceMap = Boolean(file.sourceMap);
+			// Generate Source Maps
+			sassOptions.sourceMap = Boolean(file.sourceMap);
 
-		// Update syntax to reflect the file-extension
-		if (path.extname(file.path) === '.sass') {
-			sassOptions.syntax = 'indented';
-		}
+			// Update syntax to reflect the file-extension
+			if (path.extname(file.path) === '.sass') {
+				sassOptions.syntax = 'indented';
+			}
 
-		if (async) {
-			return sass.compileStringAsync(file.contents.toString('utf-8'), {
-				...sassOptions,
-				...options || {},
-			}).then(result => {
-				return handleResult(file, result, callback);
-			}).catch(error => {
-				return callback(new PluginError(PLUGIN_NAME, error));
-			});
-		} else {
-			try {
-				const result = sass.compileString(file.contents.toString('utf-8'), {
+			if (async) {
+				return sass.compileStringAsync(file.contents.toString('utf-8'), {
 					...sassOptions,
 					...options || {},
+				}).then(result => {
+					return handleResult(file, result, callback);
+				}).catch(error => {
+					return callback(new PluginError(PLUGIN_NAME, error));
 				});
+			} else {
+				try {
+					const result = sass.compileString(file.contents.toString('utf-8'), {
+						...sassOptions,
+						...options || {},
+					});
 
-				return handleResult(file, result, callback);
-			} catch (error) {
-				return callback(new PluginError(PLUGIN_NAME, error));
+					return handleResult(file, result, callback);
+				} catch (error) {
+					return callback(new PluginError(PLUGIN_NAME, error));
+				}
 			}
-		}
+		},
 	});
-
-	return transformer;
 };
 
 module.exports = {
